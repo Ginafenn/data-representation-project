acd022f98d4794939eb56b110fb22a7b204f0a64
Ginafenn <regina.fennessy@bsci.com>
2023-12-02 08:46:21 +0000

    Update

--- PhonesDAO.py
+++ PhonesDAO.py
@@ -2,7 +2,119 @@ import mysql.connector
 import dbconfig as cfg
 
 # Using the variables from dbconfig.py
-db = mysql.connector.connect(
+
+class PhoneDAO:
+    connection = None
+    cursor = None
+    
+    def __init__(self):
+        self.host = cfg.host
+        self.user = cfg.user
+        self.password = cfg.password
+        self.database = cfg.database
+
+    def getcursor(self): 
+        self.connection = mysql.connector.connect(
+            host=       self.host,
+            user=       self.user,
+            password=   self.password,
+            database=   self.database,
+        )
+        self.cursor = self.connection.cursor()
+        return self.cursor
+
+    def closeAll(self):
+        self.connection.close()
+        self.cursor.close()
+         
+    def create(self, values):
+        cursor = self.getcursor()
+        sql="insert into phone (Make, Model, Price) values (%s,%s,%s)"
+        cursor.execute(sql, values)
+
+        self.connection.commit()
+        newid = cursor.lastrowid
+        self.closeAll()
+        return newid
+
+    def getAll(self):
+        cursor = self.getcursor()
+        sql="select * from phone"
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        returnArray = []
+        print(results)
+        for result in results:
+            print(result)
+            returnArray.append(self.convertToDictionary(result))
+        
+        self.closeAll()
+        return returnArray
+
+    def findByID(self, id):
+        cursor = self.getcursor()
+        sql="select * from phone where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+        result = cursor.fetchone()
+        returnvalue = self.convertToDictionary(result)
+        self.closeAll()
+        return returnvalue
+
+    def update(self, values):
+        cursor = self.getcursor()
+        sql="update phone set Make= %s,Model=%s, Price=%s  where id = %s"
+        cursor.execute(sql, values)
+        self.connection.commit()
+        self.closeAll()
+        
+    def delete(self, id):
+        cursor = self.getcursor()
+        sql="delete from phone where id = %s"
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        self.connection.commit()
+        self.closeAll()
+        
+        print("delete done")
+
+    def convertToDictionary(self, result):
+        colnames=['id','Make','Model', "Price"]
+        item = {}
+        
+        if result:
+            for i, colName in enumerate(colnames):
+                value = result[i]
+                item[colName] = value
+        
+        return item
+        
+phoneDAO = PhoneDAO()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" db = mysql.connector.connect(
     host=cfg.host,
     user=cfg.user,
     password=cfg.password,
@@ -20,4 +132,4 @@ db.commit()
 print("1 record inserted, ID:", cursor.lastrowid)
 
 cursor.close()
-db.close()
+db.close() """
--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from flask import Flask, url_for, redirect,request, jsonify, abort
 
+from PhonesDAO import phoneDAO
+
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
 
-phones=[
+""" phones=[
     {"id": 1, "Make": "Samsung", "Model": "s20", "Price": 500},
     {"id": 2, "Make": "Samsung", "Model": "A21", "Price": 600},
     {"id": 3, "Make": "Samsung", "Model": "S23", "Price": 800}
 ]
-nextId=4
+nextId=4 """
 
 @app.route('/')
 
@@ -16,34 +18,33 @@ def index():
 
 @app.route('/phones')
 def getAll():
-    return jsonify(phones)
+    results = phoneDAO.getAll()
+    return jsonify(results)
 
 
 # find By id
 @app.route('/phones/<int:id>')
 def findById(id):
-        foundPhones = list(filter (lambda t : t["id"]== id, phones))
-        if len(foundPhones) == 0:
-            return jsonify({}) , 204
-        return jsonify(foundPhones[0])
+    foundphone = phoneDAO.findByID(id)
+
+    return jsonify(foundphone)
 
 #curl -X POST -H "content-type:application/json" -d "{\"Make\":\"Samsung\", \"Model\":\"A54\", \"Price\":600}" http://127.0.0.1:5000/phones
 # Create
 @app.route('/phones', methods=['POST'])
 def create():
-    global nextId
+
     if not request.json:
         abort(400)
 
-    
     phone = {
-        "id": nextId,
         "Make": request.json["Make"],
         "Model": request.json["Model"],
         "Price": request.json["Price"]
     }
-    phones.append(phone)
-    nextId += 1
+    values =(phone['Make'],phone['Model'],phone['Price'])
+    newId = phoneDAO.create(values)
+    phone['id'] = newId
     return jsonify(phone)
 
 
@@ -52,29 +53,34 @@ def create():
 #curl -X PUT -d "{\"Make\":\"Nokia\", \"Model\":\"A54\", \"Price\":600}" -H "content-type:application/json" http://127.0.0.1:5000/phones/1
 @app.route('/phones/<int:id>', methods=['PUT'])
 def update(id):
-    foundPhones = list(filter(lambda t: t["id"] == id, phones))
-    if len(foundPhones) == 0:
-        return jsonify({}), 404
-    currentphone = foundPhones[0]
-    if 'Make' in request.json:
-        currentphone['Make'] = request.json['Make']
-    if 'Model' in request.json:
-        currentphone['Model'] = request.json['Model']
-    if 'Price' in request.json:
-        currentphone['Price'] = request.json['Price']
 
-    return jsonify(currentphone)
+    foundphone = phoneDAO.findByID(id)
+
+    if not foundphone:
+        abort(404)
+    
+    if not request.json:
+        abort(400)
+    reqJson = request.json
+    if 'Price' in reqJson and type(reqJson['Price']) is not int:
+        abort(400)
+
+    if 'title' in reqJson:
+        foundphone['Make'] = reqJson['Make']
+    if 'Author' in reqJson:
+        foundphone['Model'] = reqJson['Model']
+    if 'price' in reqJson:
+        foundphone['Price'] = reqJson['Price']
+    values = (foundphone['Make'],foundphone['Model'],foundphone['Price'],foundphone['id'])
+    phoneDAO.update(values)
+    return jsonify(foundphone)
 
 
 #delete
 # curl -X DELETE http://127.0.0.1:5000/phones/1
 @app.route('/phones/<int:id>', methods=['DELETE'])
 def delete(id):
-    foundPhones = list(filter(lambda t: t["id"] == id, phones))
-    if len(foundPhones) == 0:
-        return jsonify({}), 404
-    phones.remove(foundPhones[0])
-
+    phoneDAO.delete(id)
     return jsonify({"done":True})
 
 
@@ -82,5 +88,3 @@ def delete(id):
 if __name__ == "__main__":
     app.run(debug = True)
 
-if __name__ == "__main__":
-    app.run(debug=True)

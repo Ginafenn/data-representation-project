e8b4f32ff3e9e878a5d097715977de0922e61f37
Ginafenn <regina.fennessy@bsci.com>
2024-01-06 19:09:39 +0000

    update

--- dbconfig.py
+++ dbconfig.py
@@ -1,12 +1,12 @@
 # Config file
 
-""" host = "ReginaFennessy2.mysql.pythonanywhere-services.com"
+host = "ReginaFennessy2.mysql.pythonanywhere-services.com"
 user = "ReginaFennessy2"
 password = "dataroot123"  
-database = "ReginaFennessy2$data" """
+database = "ReginaFennessy2$data" 
 
 
-host = "localhost"
+""" host = "localhost"
 user = "root"
 password = "root"  
-database = "phones"
+database = "phones" """
